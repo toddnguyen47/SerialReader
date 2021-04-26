f848c0669275f1c43fc7e268952f61ad588bdbaa
Todd Nguyen <todd.nguyen47@gmail.com>
2021-04-25 19:46:39 -0700

    Add time elapsed for scripts-build.sh

--- scripts-build.sh
+++ scripts-build.sh
@@ -5,7 +5,16 @@ set -x
 
 version="latest"
 
+# Ref: https://unix.stackexchange.com/a/314370
+start_time="$(date -u +%s.%N)"
+
+# Build docker
 # docker build --tag "serial-port-reader-writer-deps:${version}" --target build-dependencies .
 # docker build --tag "serial-port-reader-writer:${version}" --target test-runner .
-
 docker build --tag "serial-port-reader-writer:${version}" .
+
+# Get elapsed time
+end_time="$(date -u +%s.%N)"
+elapsed="$(bc <<< "$end_time-$start_time")"
+echo "Total of $elapsed seconds elapsed for process"
+
